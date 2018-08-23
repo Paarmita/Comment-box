8c208135377e3dd402bf698ceae7e756ae31f7bf
paarmita <paarmita1998@gmail.com>
2018-08-23 23:54:57 +0530

    basic components added

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CommentBox from './Comments/CommentBox';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<CommentBox />, document.getElementById('root'));
+
