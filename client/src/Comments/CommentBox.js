8527defeb6d050a92f4377ac76af8cb4031fe4bb
paarmita <paarmita1998@gmail.com>
2018-08-24 09:58:29 +0530

    create get/post comments and fetch data

--- client/src/Comments/CommentBox.js
+++ client/src/Comments/CommentBox.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from '../data';
-import './CommentBox.css';
 
 class CommentBox extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      error: null,
+      author: '',
+      text: ''
+    };
+    this.pollInterval = null;
   }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    // fetch returns a promise. If you are not familiar with promises, see
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="comments">
           <h2>Comments:</h2>
-          <CommentList data={DATA} />
+          <CommentList data={this.state.data} />
         </div>
         <div className="form">
-          <CommentForm />
+          <CommentForm author={this.state.author} text={this.state.text} />
         </div>
+        {this.state.error && <p>{this.state.error}</p>}
       </div>
     );
   }
